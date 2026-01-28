89e6b9233d4a9426fbfc58dc613c0a5a89bb3550
Tejesh <d.tejeshraju67@gmail.com>
2026-01-28 18:20:10 +0530

    AgroGuide backend initial

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    password: String,
+    role: {
+        type: String,
+        default: "farmer"
+    }
+});
+
+module.exports = mongoose.model("User", userSchema);
