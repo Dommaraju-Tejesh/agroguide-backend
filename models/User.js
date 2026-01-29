707420f59e77738e3aa7c3de0504c173e5d266ff
Tejesh <d.tejeshraju67@gmail.com>
2026-01-29 16:26:22 +0530

    remove index completely

--- models/User.js
+++ models/User.js
@@ -2,11 +2,9 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     name: String,
-    phone: String,   // ❌ remove unique from here
+    phone: String,
     password: String,
     role: { type: String, default: "farmer" }
 });
-
-userSchema.index({ phone: 1 }, { unique: true }); // ✅ correct way
 
 module.exports = mongoose.model("User", userSchema);
